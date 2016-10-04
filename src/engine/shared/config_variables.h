0cf29475a4100789e8239f95e9d12472037b32c4
Tim Schumacher <tim@timakro.de>
2016-10-04 22:29:07 +0200

    Move sv_jailstate, sv_bankstate and sv_roomstate settings to map

--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -370,9 +370,9 @@ MACRO_CONFIG_INT(ClDemoShowSpeed, cl_demo_show_speed, 0, 0, 1, CFGFLAG_SAVE|CFGF
 //ChillerDragon
 MACRO_CONFIG_INT(SvKickChilliClan, sv_kick_chilli_clan, 1, 0, 1, CFGFLAG_SERVER, "Kick chilli members with wrong skin")
 
-MACRO_CONFIG_INT(SvJailState, sv_jailstate, 0, 0, 2, CFGFLAG_SERVER, "use 1 to actiavte jail")
-MACRO_CONFIG_INT(SvBankState, sv_bankstate, 0, 0, 1, CFGFLAG_SERVER, "use 1 to actiavte bank")
-MACRO_CONFIG_INT(SvRoomState, sv_roomstate, 0, 0, 1, CFGFLAG_SERVER, "use 1 to actiavte room")
+MACRO_CONFIG_INT(SvJailState, sv_jailstate, 0, 0, 2, CFGFLAG_SERVER|CFGFLAG_GAME, "use 1 to actiavte jail")
+MACRO_CONFIG_INT(SvBankState, sv_bankstate, 0, 0, 1, CFGFLAG_SERVER|CFGFLAG_GAME, "use 1 to actiavte bank")
+MACRO_CONFIG_INT(SvRoomState, sv_roomstate, 0, 0, 1, CFGFLAG_SERVER|CFGFLAG_GAME, "use 1 to actiavte room")
 
 MACRO_CONFIG_INT(SvAllowMinigame, sv_allow_minigame, 1, 0, 2, CFGFLAG_SERVER, "allow users to play minigames")
 MACRO_CONFIG_STR(SvMinigameDefaultSkin, sv_minigame_default_skin, 12, "#", CFGFLAG_SERVER, "Change default skin in minigames")
