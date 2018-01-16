c3f4e0e5601f3cd528e8060ede43ea2a02854c80
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2018-01-16 16:52:59 +0100

    test

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1,3 +1,4 @@
+//just a test
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <new>
