4567777107e08c735120bf3ca9f9518ae18138e5
ChillerDragon <chillerdragon@gmail.com>
2018-07-04 14:13:24 +0200

    Add force_skin and force_color (fixes #46)

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -619,6 +619,8 @@ private:
 	static void Condummymode(IConsole::IResult *pResult, void *pUserData);
 	static void ConDummyColor(IConsole::IResult *pResult, void *pUserData);
 	static void ConDummySkin(IConsole::IResult *pResult, void *pUserData);
+	static void ConForceColor(IConsole::IResult *pResult, void *pUserData);
+	static void ConForceSkin(IConsole::IResult *pResult, void *pUserData);
 	static void Conheal(IConsole::IResult *pResult, void *pUserData);
 	static void Conninjasteam(IConsole::IResult *pResult, void *pUserData);
 	static void ConForceUnJail(IConsole::IResult *pResult, void *pUserData);
