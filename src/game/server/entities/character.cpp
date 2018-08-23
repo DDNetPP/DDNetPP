bed13dffd7ac55f1c8f6598c8b0935d243822a8f
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2018-08-23 17:16:05 +0200

    test

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -292,6 +292,11 @@ bool CCharacter::Spawn(CPlayer *pPlayer, vec2 Pos)
 
 	m_aWeapons[0].m_Ammo = -1; //this line is added by ChillerDragon to prevent hammer in vanilla mode to run out of ammo. Im sure this solution is a bit hacky ... to who ever who is reading this comment: feel free to fix the core of the problem.
 
+	if (!m_pPlayer->m_IsSurvivaling && !m_pPlayer->m_IsVanillaWeapons)
+	{
+		m_aWeapons[1].m_Ammo = -1; // added by fokkonaut to have -1 (infinite) bullets of gun at spawn and not 10. after freeze you would have -1 anyways so why not when spawning
+	}
+
 	if (m_pPlayer->m_IsSurvivaling && !g_Config.m_SvSurvivalGunAmmo)
 	{
 		m_aWeapons[1].m_Ammo = 0;
@@ -307,10 +312,6 @@ bool CCharacter::Spawn(CPlayer *pPlayer, vec2 Pos)
 	m_pPlayer->m_SpawnRifleActive = 0;
 
 	SetSpawnWeapons();
-
-	SaveRealInfos();
-
-	UnsetSpookyGhost();
 
 	return true;
 }
@@ -2040,6 +2041,11 @@ void CCharacter::Die(int Killer, int Weapon, bool fngscore)
 	CALL_STACK_ADD();
 	dbg_msg("debug", "character die ID: %d Name: %s", m_pPlayer->GetCID(), Server()->ClientName(m_pPlayer->GetCID()));
 #endif
+
+	SaveRealInfos();
+
+	UnsetSpookyGhost();
+
 	char aBuf[256];
 	Killer = DDPP_DIE(Killer, Weapon, fngscore);
 
