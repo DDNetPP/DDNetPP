b1ee7e5ee3fcb5e71ed1209cdb623207cdd9f75a
ChillerDragon <chillerdragon@gmail.com>
2019-01-20 19:49:06 +0100

    plants alpha test

---
+++ src/game/server/entities/plant.h
@@ -0,0 +1,34 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#ifndef GAME_SERVER_ENTITIES_PICKUP_H
+#define GAME_SERVER_ENTITIES_PICKUP_H
+
+#include <game/server/entity.h>
+
+const int PickupPhysSize = 14;
+
+class CPlant : public CEntity
+{
+public:
+	CPlant(CGameWorld *pGameWorld);
+
+	virtual void Reset();
+	virtual void Tick();
+	virtual void TickPaused();
+	virtual void Snap(int SnappingClient);
+
+	int GetType() { return m_Type; }
+
+private:
+
+	int m_Type;
+	int m_Subtype;
+	int m_SpawnTick;
+
+	// DDRace
+
+	void Move();
+	vec2 m_Core;
+};
+
+#endif
