ede0509fe2ffb58993469e62e7f9d5b6bd5ffc25
ChillerDragon <chillerdragon@gmail.com>
2019-05-12 16:19:10 +0200

    Init survivial gamestate to zero

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -8806,6 +8806,7 @@ void CGameContext::OnInit(/*class IKernel *pKernel*/)
 	m_BalanceID1 = -1;
 	m_BalanceID2 = -1;
 	m_BlockWaveGameState = 0;
+	m_survival_gamestate = 0;
 	m_CucumberShareValue = 10;
 	m_BombTick = g_Config.m_SvBombTicks;
 	m_BombStartCountDown = g_Config.m_SvBombStartDelay;
