0281ba2977519c700c2a74794fb4680a960d77c8
ChillerDragon <ChillerDragon@gmail.com>
2019-08-19 18:26:31 +0200

    Add blockwave test cheats

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -5245,13 +5245,19 @@ void CGameContext::ConTCMD3000(IConsole::IResult *pResult, void *pUserData)
 
 	if (g_Config.m_SvTestingCommands)
 	{
+		/*
         pSelf->m_MissionUnlockedLevel = pResult->GetInteger(0);
         pSelf->m_MissionCurrentLevel = pResult->GetInteger(1);
         str_format(aBuf, sizeof(aBuf), "updated level unlocked=%d current=%d", pSelf->m_MissionUnlockedLevel, pSelf->m_MissionCurrentLevel);
         pSelf->SendChatTarget(pResult->m_ClientID, aBuf);
         pSelf->SaveSinglePlayer();
-        
-        
+		*/
+
+		pSelf->m_BlockWaveRound = pResult->GetInteger(0);
+		str_format(aBuf, sizeof(aBuf), "set blockwave level %d", pSelf->m_BlockWaveRound);
+		pSelf->SendChatTarget(pResult->m_ClientID, aBuf);
+		pSelf->BlockWaveWonRound();
+
 		/*
 		if (pResult->NumArguments() != 2)
 		{
@@ -5268,17 +5274,6 @@ void CGameContext::ConTCMD3000(IConsole::IResult *pResult, void *pUserData)
 
 		//pSelf->ChillUpdateFileAcc(pResult->GetString(0), pResult->GetInteger(1), pResult->GetString(2), pResult->m_ClientID); //a fully working set all values of acc2 files but its a bit op maybe add it to the rcon api but not as normal admin cmd
 	}
-
-	/*
-	if (pPlayer->m_IsInstaArena_gdm)
-	{
-		pSelf->SendChatTarget(pResult->m_ClientID, aBuf);
-	}
-	else
-	{
-		pSelf->SendChatTarget(pResult->m_ClientID, "NOT IN BOOMFNG");
-	}
-	*/
 }
 
 void CGameContext::ConAntiFlood(IConsole::IResult * pResult, void * pUserData)
