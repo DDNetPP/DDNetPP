766fce925ab7569df21eb690cd78368d58f5cf15
ChillerDragon <chillerdragon@gmail.com>
2020-08-01 21:09:25 +0200

    Remove useless commit script

--- auto_commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#much wow bash script to super simplyfy git commands
-#mede by ChillerDragon for nobody
-
-read -p "Commit message: " commit_msg
-
-git add .
-git commit -m "$commit_msg"
-git push
