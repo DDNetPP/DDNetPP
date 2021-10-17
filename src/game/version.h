7d201d144a9db72be27aa39983e7a09474aab171
ChillerDragon <ChillerDragon@gmail.com>
2021-10-17 21:38:51 +0200

    Use own version file to avoid damn conflicts omagawd

--- src/game/version.h
+++ src/game/version.h
@@ -1,5 +1,8 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
+
+#include "version_ddpp.h"
+
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
@@ -7,18 +10,6 @@
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 static const char GAME_RELEASE_VERSION[8] = "10.3";
 #define CLIENT_VERSIONNR 1003
-
-// ddnet++
-
-#define DDNETPP_VERSION "v.0.0.8-dev"
-#define GAME_RELEASE_VERSION = "9.0.2";
-// extern const char *GIT_SHORTREV_HASH;
-
-// VARCHAR(128) in the database
-#define MIN_PW_LEN 3
-#define MAX_PW_LEN 120
-
-#define MIN_PW_LEN_STR "3"
-#define MAX_PW_LEN_STR "120"
-
 #endif
+
+#define GAME_RELEASE_VERSION = "9.0.2"; // TODO: remove
---
+++ src/game/version_ddpp.h
@@ -0,0 +1,16 @@
+// ddnet++
+
+#ifndef GAME_VERSION_DDPP_H
+#define GAME_VERSION_DDPP_H
+
+#define DDNETPP_VERSION "v.0.0.8-dev"
+// extern const char *GIT_SHORTREV_HASH;
+
+// VARCHAR(128) in the database
+#define MIN_PW_LEN 3
+#define MAX_PW_LEN 120
+
+#define MIN_PW_LEN_STR "3"
+#define MAX_PW_LEN_STR "120"
+
+#endif
