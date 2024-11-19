830c6a6439f8a90a56327daf90f9dd0171561722
ChillerDragon <chillerdragon@gmail.com>
2024-11-19 11:27:15 +0800

    Fix a bit of style

--- src/engine/shared/console_ddpp.cpp
+++ src/engine/shared/console_ddpp.cpp
@@ -1,5 +1,3 @@
-#include <new>
-
 #include <base/color.h>
 #include <base/ddpp_logs.h>
 #include <base/math.h>
@@ -9,21 +7,18 @@
 #include <engine/shared/protocol.h>
 #include <engine/storage.h>
 
-#include "config.h"
 #include "console.h"
-#include "linereader.h"
 
-void CConsole::PrintDDPPLogs(int type)
+void CConsole::PrintDDPPLogs(int Type)
 {
-	/*
-        start from the oldest log
-        so the last line printed in the console is the latest log
-        scroll up to go in the past
-    */
+	// start from the oldest log
+	// so the last line printed in the console is the latest log
+	// scroll up to go in the past
 	for(int i = DDPP_LOG_SIZE - 1; i >= 0; i--)
 	{
-		if(!aDDPPLogs[type][i][0])
+		if(!aDDPPLogs[Type][i][0])
 			continue;
-		Print(OUTPUT_LEVEL_STANDARD, "ddpp_logs", aDDPPLogs[type][i]);
+		Print(OUTPUT_LEVEL_STANDARD, "ddpp_logs", aDDPPLogs[Type][i]);
 	}
 }
+
