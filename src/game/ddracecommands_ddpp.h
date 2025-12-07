758027e075681cbf2ff2804e4566a0ca463c7a01
ChillerDragon <chillerdragon@gmail.com>
2025-12-07 11:28:10 +0100

    ++ hide_player named arg

--- src/game/ddracecommands_ddpp.h
+++ src/game/ddracecommands_ddpp.h
@@ -72,7 +72,7 @@ CONSOLE_COMMAND("freeze", "v[id] ?i", CFGFLAG_SERVER, ConFreeze, this, "Freezes 
 // HACK COMMAND ChillerDragon
 // CONSOLE_COMMAND("hack", "vi", CFGFLAG_SERVER, ConHack, this, "dont use this command!")
 CONSOLE_COMMAND("godmode", "v[id]", CFGFLAG_SERVER, ConGodmode, this, "gives player i godmode (no damage in instagib)")
-CONSOLE_COMMAND("hide_player", "v", CFGFLAG_SERVER, ConHidePlayer, this, "makes player invisible")
+CONSOLE_COMMAND("hide_player", "v[id]", CFGFLAG_SERVER, ConHidePlayer, this, "makes player invisible")
 CONSOLE_COMMAND("verify_player", "v", CFGFLAG_SERVER, ConVerifyPlayer, this, "manually set a targets player human level to max (force solve captcha)")
 
 CONSOLE_COMMAND("logs", "?s[type]", CFGFLAG_SERVER, ConDDPPLogs, this, "shows ddnet++ logs (types: mastersrv)")
