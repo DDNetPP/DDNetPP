a97d7431f5a6e867406b745c48ece04c100a187d
ChillerDragon <chillerdragon@gmail.com>
2025-12-08 12:49:16 +0100

    ++ some more named args

--- src/game/ddracecommands_ddpp.h
+++ src/game/ddracecommands_ddpp.h
@@ -22,12 +22,12 @@ CONSOLE_COMMAND("inf_trail", "v", CFGFLAG_SERVER, ConInfTrail, this, "activates 
 
 CONSOLE_COMMAND("homing_missile", "v", CFGFLAG_SERVER, ConHomingMissile, this, "toggles homing missile activate/deactive (grenade launcher)")
 
-CONSOLE_COMMAND("disarm", "v", CFGFLAG_SERVER, Condisarm, this, "disarm a evil tee to prevent him doing evil stuff :)")
-CONSOLE_COMMAND("dummymode", "vi", CFGFLAG_SERVER, Condummymode, this, "0 default, 23 cb5(racer), 29 cb5(blocker), 103 ctf5, 27 blmapchill(policebot), 32 blmapchill(solo policebot)")
-CONSOLE_COMMAND("dummy_color", "vi", CFGFLAG_SERVER, ConDummyColor, this, "changes the color of a specific dummy")
-CONSOLE_COMMAND("dummy_skin", "vr", CFGFLAG_SERVER, ConDummySkin, this, "changes the skin of a specific dummy")
-CONSOLE_COMMAND("force_color", "vi", CFGFLAG_SERVER, ConForceColor, this, "changes the color of a specific player")
-CONSOLE_COMMAND("force_skin", "vr", CFGFLAG_SERVER, ConForceSkin, this, "changes the skin of a specific player")
+CONSOLE_COMMAND("disarm", "v[id]", CFGFLAG_SERVER, Condisarm, this, "disarm a evil tee to prevent him doing evil stuff :)")
+CONSOLE_COMMAND("dummymode", "v[id] i[mode]", CFGFLAG_SERVER, Condummymode, this, "0 default, 23 cb5(racer), 29 cb5(blocker), 103 ctf5, 27 blmapchill(policebot), 32 blmapchill(solo policebot)")
+CONSOLE_COMMAND("dummy_color", "v[id] i[color]", CFGFLAG_SERVER, ConDummyColor, this, "changes the color of a specific dummy")
+CONSOLE_COMMAND("dummy_skin", "v[id] r[skin]", CFGFLAG_SERVER, ConDummySkin, this, "changes the skin of a specific dummy")
+CONSOLE_COMMAND("force_color", "v[id] i[color]", CFGFLAG_SERVER, ConForceColor, this, "changes the color of a specific player")
+CONSOLE_COMMAND("force_skin", "v[id] r[skin]", CFGFLAG_SERVER, ConForceSkin, this, "changes the skin of a specific player")
 CONSOLE_COMMAND("heal", "v", CFGFLAG_SERVER, Conheal, this, "heals a tee's Health to 10 hp")
 CONSOLE_COMMAND("force_unjail", "v[player]", CFGFLAG_SERVER, ConForceUnJail, this, "unjails player instantly")
 CONSOLE_COMMAND("force_jail", "v[player]i[seconds]", CFGFLAG_SERVER, ConForceJail, this, "jails player by adminforce")
